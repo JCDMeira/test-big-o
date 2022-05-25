96f38278c00f56ba75daf0b18240896f0cb05ffb
Jean Meira <jean.meira10@hotmail.com>
2022-05-25 19:00:19 -0300

    doc: add explanation

--- 2-notationLog.js
+++ 2-notationLog.js
@@ -1,4 +1,13 @@
 //* referência -> https://gabrielschade.github.io/2019/06/16/algoritmos-i.html
+/*
+    _ Vai passar uma vez por cada bloco do código um número de vezes que tende
+    _ a estabilizar, não crescendo tanto para grandes volumes.
+    _ o log apresenta um aumento no início e acaba tendo aumentos em ritmos
+    _ menores após certo tempo.
+    _ Sendo que isso ocorre porque tende a fazer um número de iterações baseados
+    _ no volume de dados, mas não é uma quantidade que cresce tanto, como nas
+    _ buscas binárias de arrays ordenados (caso abaixo)
+*/
 
 const array = [];
 
